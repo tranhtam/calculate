6c8ddae569792c9be66ee2ebfc7faa2512fbe978
tam <tranteo25299@gmail.com>
2022-05-18 16:02:37 +0700

    update input

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 function App() {
 
-  const [result, setResult] = useState('_')
+  const [result, setResult] = useState('0')
+  const [fakeResult, setFakeResult] = useState('0')
   const [isCalculate, setIsCalculate] = useState(false)
   const [isFirstRender, setIsFirstRender] = useState(true)
   const [isLunisolar, setIsLunisolar] = useState(true)
@@ -11,13 +12,39 @@ function App() {
   const [isPercent, setIsPercent] = useState(false)
 
   const handleClick = (e) => {
-    if (e.target.name !== '_' && isFirstRender) {
-      setResult(result.replace('_', e.target.name))
+    // if (e.target.name !== '0' && isFirstRender) {
+    //   setResult(result.replace('0', e.target.name))
+    //   setFakeResult(result.replace('0', e.target.name))
+    //   setIsCalculate(true)
+    //   setIsFirstRender(false)
+    //   setIsPercent(true)
+    // } else {
+    //   setResult(result.concat(e.target.name))
+    //   setFakeResult(fakeResult.concat(e.target.name))
+    //   setIsCalculate(true)
+    //   setIsFirstRender(false)
+    //   setIsPercent(true)
+    //   // setFakeResult(result.replace(result, e.target.name))
+    // }
+
+    if (e.target.name !== '0' && isFirstRender) {
+      setResult(result.replace('0', e.target.name))
+      setFakeResult(result.replace('0', e.target.name))
+      setIsCalculate(true)
+      setIsFirstRender(false)
+      setIsPercent(true)
+    } else if (result.substr(-1) === '-'
+      || result.substr(-1) === '+'
+      || result.substr(-1) === '/'
+      || result.substr(-1) === '*') {
+      setFakeResult(result.replace(result, e.target.name))
+      setResult(result.concat(e.target.name))
       setIsCalculate(true)
       setIsFirstRender(false)
       setIsPercent(true)
     } else {
       setResult(result.concat(e.target.name))
+      setFakeResult(fakeResult.concat(e.target.name))
       setIsCalculate(true)
       setIsFirstRender(false)
       setIsPercent(true)
@@ -29,7 +56,7 @@ function App() {
       setResult(result.concat(e.target.name))
       setIsCalculate(false)
       setIsDot(true)
-
+      setIsPercent(false)
     }
     // else {
     //   setResult(result.replace('-', e.target.name))
@@ -39,7 +66,8 @@ function App() {
   }
 
   const handleClear = () => {
-    setResult('_')
+    setResult('0')
+    setFakeResult('0')
     setIsCalculate(false)
     setIsFirstRender(true)
     setIsLunisolar(true)
@@ -50,20 +78,25 @@ function App() {
   const handleEqualPercent = () => {
     if (isPercent) {
       setResult(eval(result / '100').toString())
+      setFakeResult(eval(result / '100').toString())
     }
   }
 
   const handleEqual = (e) => {
     setResult(eval(result).toString())
+    setFakeResult(eval(result).toString())
+    setIsPercent(false)
     setIsDot(false)
   }
 
   const handleLunisolar = (e) => {
     if (isLunisolar) {
       setResult('-' + result.concat(e.target.name))
+      setFakeResult('-' + result.concat(e.target.name))
       setIsLunisolar(false)
     } else {
       setResult(result.replace('-', e.target.name))
+      setFakeResult(result.replace('-', e.target.name))
       setIsLunisolar(true)
     }
   }
@@ -71,6 +104,7 @@ function App() {
   const handleDot = (e) => {
     if (isDot) {
       setResult(result.concat(e.target.name) + '.')
+      setFakeResult(fakeResult.concat(e.target.name) + '.')
       setIsDot(false)
     }
   }
@@ -86,7 +120,9 @@ function App() {
       </div>
 
       <form>
-        <input type='text' value={result} />
+        {console.log("result: ", result)}
+        {console.log("fakeResult: ", fakeResult)}
+        <input type='text' value={fakeResult} />
       </form>
 
       <div className='keypad'>
@@ -110,7 +146,6 @@ function App() {
         <button onClick={handleDot} className='light-gray'>,</button>
         <button onClick={handleEqual} id='equal' className='orange'>=</button>
       </div>
-      {/* <button></button> */}
     </div>
   );
 }
